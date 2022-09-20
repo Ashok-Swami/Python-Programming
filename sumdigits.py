a6836149e8abd74b746f6f499770770a397b342b
Ashok Swami <ashokswami1137@gmail.com>
2022-09-20 19:37:37 +0530

    initial commit

---
+++ sumdigits.py
@@ -0,0 +1,12 @@
+def getSum(n):
+    sum = 0
+    while (n != 0):
+        sum = sum + int(n % 10)
+        n = int(n/10)
+    return sum
+
+if __name__ == "__main__":
+    n = 687
+ 
+    # Function call
+    print(getSum(n))
